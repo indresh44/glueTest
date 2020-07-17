2a37755991a23b49d07f9a2f2e23971f5be8f8e5
ikrock <kindresh44@gmail.com>
2020-07-17 17:17:27 +0530

    unit test added

---
+++ tests/glue_test.py
@@ -0,0 +1,36 @@
+import unittest
+import sys
+import boto3
+import filecmp
+import time
+
+glue = boto3.client('glue')
+
+
+def runJob(jobname):
+	response = glue.start_job_run(JobName=jobname)
+	jobRunid = response['JobRunId']
+	response = glue.get_job_run(JobName=jobname,RunId=jobRunid)
+	state = response['JobRun']['JobRunState']
+	print "state " + state
+	while state == 'RUNNING':
+		time.sleep(180)
+		response = glue.get_job_run(JobName=jobname,RunId=jobRunid)
+		state = response['JobRun']['JobRunState']
+		print "state " + state
+	print "final state " + state
+	return state
+
+class MyTestCase(unittest.TestCase):
+  def test_glue(self):
+
+      # Remove the hardcoded value, take as a parameter. Right now hardcoded for demo purpose.
+      job_name = "GlueCsvToParq"
+
+      self.assertEqual(runJob(job_name), 'SUCCEEDED')
+
+
+if __name__ == '__main__':
+  # if len(sys.argv) > 1:
+  #       MyTestCase.STACKNAME = sys.argv.pop()
+  unittest.main()
